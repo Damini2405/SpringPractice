17de48d023412acf54e85e9ab47f5873b5d0480c
Damini2405 <42958623+Damini2405@users.noreply.github.com>
2018-09-15 13:21:10 +0530

    project added

---
+++ GitPractice/src/main/java/com/git/GitPractice/App.java
@@ -0,0 +1,13 @@
+package com.git.GitPractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
